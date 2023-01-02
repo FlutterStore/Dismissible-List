f68d5d74fce02ce3272518afb235711fc18b0ae7
Flutter Store <fluttertutorialstore@gmail.com>
2023-01-02 18:57:39 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/dismissible_list/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dismissible_list
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key,});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+   List<int> items = List<int>.generate(100, (int index) => index);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Dismissible List",style: TextStyle(fontSize: 15),),
+      ),
+      body: ListView.builder(
+        itemCount: items.length,
+        padding: const EdgeInsets.symmetric(vertical: 16),
+        itemBuilder: (BuildContext context, int index) {
+          return Dismissible(
+            background: Container(
+              color: Colors.red,
+              alignment: Alignment.center,
+              child: const Text("Delete",style: TextStyle(fontSize: 15,color: Colors.white),),
+            ),
+            key: ValueKey<int>(items[index]),
+            onDismissed: (DismissDirection direction) {
+              setState(() {
+                items.removeAt(index);
+              });
+            },
+            child: ListTile(
+              title: Text(
+                'Item ${items[index]}',
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
